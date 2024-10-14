58d6e1f4034cb53d2e27c7a92040ca3f530a6c77
Saryu Saraswat <21cse107@gweca.ac.in>
2024-10-14 14:33:07 +0530

    added task 48

---
+++ Javascript/Arrays/Task48/script.js
@@ -0,0 +1,27 @@
+//Custom Array Methods
+
+
+//forEachArray function
+function forEachArray(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i], i, array);
+    }
+}
+
+function mapArray(array, callback) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i], i, array));
+    }
+    return result;
+}
+
+function filterArray(array, callback) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
